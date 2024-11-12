18462cdfbc36a4d75c8f651fccf2c6ce681117f8
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-11-12 13:46:40 -0500

    Finished Assignment.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+       Payment creditCard = new CreditCardPayment();
+       Payment paypal = new PayPalPayment();
+       Payment bankAccount = new BankTransferPayment();
+
+        processPayment(creditCard, 150);
+        processPayment(paypal, 200.50);
+        processPayment(bankAccount, 1000.0);
+
+    }
+
+    public static void processPayment(Payment paymentMethod, double amount){
+        paymentMethod.processPayment(amount);
+    }
 }
